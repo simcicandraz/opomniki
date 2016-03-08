9671b932d2094609895f2a7e5c3e88ad6897e6f8
Andraz Simcic <as7495@student.uni-lj.si>
2016-03-08 13:19:42 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -8,8 +8,20 @@ window.addEventListener('load', function() {
 		document.querySelector(".pokrivalo").style.visibility = "hidden";
 	}
 	
-	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	var dodajOpomnik = function(event) {
+		var nazivZadovzitve = document.querySelector("#naziv_opomnika").value;
+		var casOpomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		document.querySelector("#opomniki").innerHTML += "\
+			<div class='opomnik'> \
+            	<div class='naziv_opomnika'>" + nazivZadovzitve + "</div> \
+            	<div class='cas_opomnika'> Opomnik čez <span>" + casOpomnika + "</span> sekund.</div> \
+			</div>";
+	}
 	
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
